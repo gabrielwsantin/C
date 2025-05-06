5f6b7da6ec42249a8ff1b6833d961591a94c783e
Gabriel S. <gabrielsas1809@gmail.com>
2025-05-06 09:48:39 -0300

    nome sobrenome
    Faça um programa que pede o primeiro nome do usuário e um sobrenome e mostra os dois nomes digitados na tela.

---
+++ nome sobrenome.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char primeiroNome[50], sobrenome[50];
+
+    // Solicita o primeiro nome e o sobrenome
+    printf("Digite o primeiro nome: ");
+    scanf("%s", primeiroNome);
+
+    printf("Digite o sobrenome: ");
+    scanf("%s", sobrenome);
+
+    // Exibe os nomes na tela
+    printf("Primeiro nome: %s\n", primeiroNome);
+    printf("Sobrenome: %s\n", sobrenome);
